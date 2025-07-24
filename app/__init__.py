c7498ddad2f6c11a8f75dabe39cff0bd5e264675
Abbie <abbie.sims@canonical.com>
2025-07-24 10:22:30 +0400

    add base dashboard layout

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,19 @@ from flask import Flask
 from config import Config
 from app.extensions import db, migrate
 from app.public.api import public_bp
+from app.dashboard.routes import dashboard_bp
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     app.config.from_object(Config)
-
-    @app.route("/")
-    def index():
-        return "Charmhub Solutions API - Copyright 2025 Canonical"
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     with app.app_context():
         app.register_blueprint(public_bp, url_prefix="/api")
+        app.register_blueprint(dashboard_bp, url_prefix="/")
 
     return app
 
