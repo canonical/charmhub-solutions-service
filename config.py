aa410a56fcf642f59e8843d6ce5efb85ec5e21b7
Mohammad Iskandarany <m7md.k.isk@gmail.com>
2025-07-30 12:02:35 +0300

    update config

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///solutions.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRESQL_DB_CONNECT_STRING', 'sqlite:///solutions.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
