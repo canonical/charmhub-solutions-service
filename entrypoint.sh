d12191e1294ce5dfeac6da0e8ce04437a1ec16fc
Abbie Sims <abbie.sims@canonical.com>
2026-06-05 14:57:43 +0400

    add db readiness check

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 # Exit on error
 set -e
 
+# Wait for database to accept connections
+until python -c 'import os, psycopg2; psycopg2.connect(os.environ["POSTGRESQL_DB_CONNECT_STRING"]).close()'; do
+  echo "Waiting for database..."
+  sleep 1
+done
+
 # Apply database migrations
 flask db upgrade
 
